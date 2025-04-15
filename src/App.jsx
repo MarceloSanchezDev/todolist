d9bfa14e267ae1c07acec8ed9abc621a60da0074
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-15 00:32:42 -0300

    feat: add Login and Register components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Login from "./components/login.jsx";
+import Register from "./components/register.jsx";
+
 function App() {
   return (
     <>
       <h1>TO-DO LIST</h1>
+      <Login></Login>
+      <Register></Register>
     </>
   );
 }
