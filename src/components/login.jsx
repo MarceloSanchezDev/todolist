8e080a768a865b50f4a756fb92eb784112ebf106
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-15 01:10:19 -0300

    feat: implement Login and Register components with form handling

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+
 export default function Login(params) {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("username", username, "password", password);
+  };
   return (
     <>
-      <h1>Login</h1>
+      <div className="container">
+        <h1 className="text-center">Login</h1>
+        <form className="form-group text-center" onSubmit={handleSubmit}>
+          <div className="row d-flex justify-content-center align-items-center border rounded flex-column text-start">
+            <div className="col-4 mb-3">
+              <label htmlFor="username" className="form-label p-2">
+                Username :
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="form-control"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="col-4 mb-3">
+              <label htmlFor="password" className="form-label p-2">
+                Password :
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="col-4 mb-3">
+              <button type="submit" className="btn btn-success">
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,7 +1,93 @@
+import { useState } from "react";
+
 export default function Register(params) {
+  const [newUser, setNewUser] = useState({
+    email: null,
+    username: null,
+    password: null,
+    confirmPassword: null,
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, username, password, confirmPassword } = newUser;
+    console.log(
+      "email",
+      email,
+      "username",
+      username,
+      "password",
+      password,
+      "confirmPassword",
+      confirmPassword
+    );
+  };
   return (
     <>
-      <h1>Register</h1>
+      <div className="container">
+        <h1 className="text-center">Register</h1>
+        <form className="form-group text-center" onSubmit={handleSubmit}>
+          <div className="row d-flex justify-content-center align-items-center border rounded flex-column text-start">
+            <div className="col-4">
+              <label htmlFor="email" className="label-form p-2">
+                E-mail :
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                required
+                onChange={(e) =>
+                  setNewUser({ ...newUser, email: e.target.value })
+                }
+              />
+            </div>
+            <div className="col-4">
+              <label htmlFor="username" className="label-form p-2">
+                Username :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={(e) =>
+                  setNewUser({ ...newUser, username: e.target.value })
+                }
+              />
+            </div>
+            <div className="col-4">
+              <label htmlFor="" className="label-form p-2">
+                Password :
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                required
+                onChange={(e) =>
+                  setNewUser({ ...newUser, password: e.target.value })
+                }
+              />
+            </div>
+            <div className="col-4">
+              <label htmlFor="" className="label-form p-2">
+                Confirm Password :
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                required
+                onChange={(e) =>
+                  setNewUser({ ...newUser, confirmPassword: e.target.value })
+                }
+              />
+            </div>
+            <div className="col-4">
+              <button type="submit" className="btn btn-success mt-3">
+                Register
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
