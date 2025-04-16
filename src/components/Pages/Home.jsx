4824e7f3c57f2348a22945ec497bf4ba19c027d3
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-16 18:53:15 -0300

    feat: update Home component header; swap title and subtitle for improved visibility

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -8,8 +8,7 @@ export default function Home() {
     <div className="container-fluid d-flex flex-column justify-content-center align-items-center min-vh-100 bg-dark text-light">
       <div className="container d-flex flex-column justify-content-center align-items-center p-4 row">
         <div className="col-12 mb-3 d-flex flex-column justify-content-center align-items-center">
-          <h1 className="text-center">Home</h1>
-          <p className="text-center">✅ TO-DO APP!</p>
+          <h1 className="text-center">✅ TO-DO APP!</h1>
           <Button
             bootstrapClass={"btn btn-success"}
             onClick={() => navigate("/app")}
