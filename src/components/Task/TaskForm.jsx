e898c534dccf765c490b4c1f7e7477d12b58295d
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-19 19:31:20 -0300

    feat: add validation to task submission; ensure task name is at least 5 characters feat: initialize API authentication routes for login and registration feat: create API index file for server entry point chore: add package.json for API configuration

--- src/components/Task/TaskForm.jsx
+++ src/components/Task/TaskForm.jsx
@@ -5,14 +5,15 @@ export default function TaskForm({ setTaskList }) {
   const [task, setTask] = useState({ name: "", completed: false });
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTaskList((prevTasks) => [...prevTasks, task]);
-    setTask({ name: "", completed: false });
+    if (task.name.length > 4) {
+      setTaskList((prevTasks) => [...prevTasks, task]);
+      setTask({ name: "", completed: false });
+    } else {
+      console.log("la terea no puede ser menor a 4 caracteres");
+    }
   };
   return (
-    <form
-      className="row mb-3 w-100 "
-      onSubmit={handleSubmit}
-    >
+    <form className="row mb-3 w-100 " onSubmit={handleSubmit}>
       <input
         type="text"
         className="rounded me-2  col col-sm-8"
