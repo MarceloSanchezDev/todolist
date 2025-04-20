de9fb0ea915d1bd78403a63353b04b65115f8eb1
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-19 23:12:10 -0300

    feat: validate user registration data before responding in API handler

--- api/auth/register.js
+++ api/auth/register.js
@@ -1,7 +1,11 @@
+import {validRegisterUser} from '../schema/userSchema.js'
+
 export default async function handler(req, res) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Método no permitido" });
     }
     const {body} = req;
-    return res.status(200).json({ message: "Hola desde la API" ,body});
+    const userValid = validRegisterUser(body)
+
+    return res.status(200).json({ message: "Hola desde la API" ,userValid});
 }
