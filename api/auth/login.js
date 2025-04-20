487c88b14e2bafdeccb73a78564f9926ee3aed7a
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-19 22:07:37 -0300

    fix: remove userData validation and return body in login API response

--- api/auth/login.js
+++ api/auth/login.js
@@ -2,9 +2,6 @@ export default async function handler(req, res) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Método no permitido" });
     }
-    const { userData } = req.body;
-    if (!userData) {
-        return res.status(400).json({ error: "Username is required" });
-    }
-    return res.status(200).json({ message: "Hola desde la API" , userData});
+    const {body} = req;
+    return res.status(200).json({ message: "Hola desde la API" , body});
 }
