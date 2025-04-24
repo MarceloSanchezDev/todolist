98631868e1a2c00bb511f48e199acb223cbd90d3
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-24 13:16:04 -0300

    fix: refactor allTask handler to improve error handling and code structure

--- api/task/allTask.js
+++ api/task/allTask.js
@@ -5,13 +5,18 @@ export default async function handler(req, res) {
         return res.status(405).json({ message: "Method Not Allowed" });
       }
     const { body } = req;
-    
+    try {
+      
+      const { user } = body;
+      const { username } = user;
+      const tasks = await TaskModel.getAllTasks(username);
+      const tasksCompleted = await TaskModel.getAllTasksCompleted(username);
+  
+      res.status(200).json({ tasks, tasksCompleted });
+    } catch (error) {
+        console.error("Error en allTask:", error);
+        res.status(500).json({ error: "Error interno del servidor" });      
+    }
 
-        const { user } = body;
-        const { username } = user;
-        const tasks = await TaskModel.getAllTasks(username);
-        const tasksCompleted = await TaskModel.getAllTasksCompleted(username);
-
-        res.status(200).json({ tasks, tasksCompleted });
    
 }
