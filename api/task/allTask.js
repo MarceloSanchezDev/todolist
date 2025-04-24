6efd8175704a588ee8b0733e154e5d9b1faf8c2f
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-23 21:06:03 -0300

    fix: comment out task retrieval logic in allTask handler

--- api/task/allTask.js
+++ api/task/allTask.js
@@ -6,11 +6,13 @@ export default async function handler(req, res) {
     }
     const { body } = req;
     try {
+        /*
         const { user } = body;
         const { username } = user;
         const tasks = await UserModel.getAllTasks(username);
         const tasksCompleted = await UserModel.getAllTasksCompleted(username);
-        res.status(200).json({ tasks, tasksCompleted });
+        */
+        res.status(200).json({ body});
     } catch (error) {
         return res.status(400).json({ message: "Error al obtener las tareas" , error});
     }
