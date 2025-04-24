16ddab093fb865285072b717386a948bef9bc87d
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-23 21:07:41 -0300

    fix: remove method check for POST in allTask handler

--- api/task/allTask.js
+++ api/task/allTask.js
@@ -1,9 +1,6 @@
 import { UserModel } from "../models/turso/userTask.js";
 
 export default async function handler(req, res) {
-    if (req.method !== "POST") {
-        return res.status(405).json({ error: "Método no permitido" });
-    }
     const { body } = req;
     try {
         /*
