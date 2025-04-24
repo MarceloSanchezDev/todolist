ec64c15d0bc8515d99eaf436a65f7ff0892d02ae
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-23 21:25:42 -0300

    fix: restore task fetching logic in allTask handler

--- api/task/allTask.js
+++ api/task/allTask.js
@@ -6,13 +6,13 @@ export default async function handler(req, res) {
       }
     const { body } = req;
     try {
-        /*
+
         const { user } = body;
         const { username } = user;
         const tasks = await UserModel.getAllTasks(username);
         const tasksCompleted = await UserModel.getAllTasksCompleted(username);
-        */
-        res.status(200).json({ body});
+
+        res.status(200).json({ tasks, tasksCompleted });
     } catch (error) {
         return res.status(400).json({ message: "Error al obtener las tareas" , error});
     }
