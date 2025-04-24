e65fa1e48ebd258b377defc24de3343059234f37
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-24 13:24:29 -0300

    fix: add console log for taskList, taskCompleted, and taskDeleted in TaskProvider

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -10,6 +10,7 @@ export const TaskProvider = ({ children }) => {
   const [taskCompleted, setTaskCompleted] = useState([]);
   const [taskDeleted, setTaskDeleted] = useState([]);
   const { user } = useAuthContext();
+  console.log(taskList, taskCompleted, taskDeleted);
   useEffect(() => {
     const getAllTasks = async () => {
       try {
