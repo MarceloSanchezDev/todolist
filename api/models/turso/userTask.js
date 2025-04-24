c3a68a7bd2e92433445c7b1ab4cdf8a9d804cba2
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-04-23 21:52:39 -0300

    fix: enhance error logging in getAllTasks and createTask methods

--- api/models/turso/userTask.js
+++ api/models/turso/userTask.js
@@ -45,6 +45,7 @@ export class TaskModel {
             const {rows} = await db.execute('SELECT * FROM user_task WHERE user_username = ?', [username]);
             return rows[0]
         }catch (error) {
+          console.error("Error al obtener las tareas:", error);
             return `Error getAllTask : ${error}`
         }
     }
@@ -66,6 +67,7 @@ export class TaskModel {
             const {rows} = await db.execute('SELECT * FROM user_task WHERE user_username = ?', [username]);
             return rows[0]
         }catch (error) {
+          console.error("Error al crear la tarea:", error);
             return `Error createTask : ${error}`
         }
     }
